c398c4d76341c04a0fda03ca30be14834bc69bc5
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-09-15 18:32:48 -0700

    Time: 1 ms (81.76%) | Memory: 40.9 MB (43.80%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,32 +1,18 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> bank = new HashMap<>();
+        Set<Character> jewelType = new HashSet<>();
+        int output = 0;
 
-        char[] stonesList = stones.toCharArray();
+        for (int i = 0; i < jewels.length(); i++) {
+            jewelType.add(jewels.charAt(i));
+        }
 
-        for (char stone : stonesList) {
-            if (bank.containsKey(stone)) {
-                bank.put(stone, bank.get(stone) + 1);
-            } else {
-                bank.put(stone, 1);
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewelType.contains(stones.charAt(i))) {
+                output++;
             }
         }
 
-        char[] jewelsList = jewels.toCharArray();
-        int count = 0;
-
-        for (char jewel : jewelsList) {
-            if (bank.containsKey(jewel)) {
-                count += bank.get(jewel);
-                bank.put(jewel, 0);
-            }
-        }
-
-        return count;
-
+        return output;
     }
-
 }
-
-// Time O(n) whichever is longer jewles or stones becase we iterate over each list once
-// Space O(n) becase we make the char arrays and hashmap
