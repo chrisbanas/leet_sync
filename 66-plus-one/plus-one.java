44c8d364e7f79c79469fe5869fa69825683cc0f8
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-10-15 22:20:39 -0700

    Time: 0 ms (100.00%) | Memory: 40.9 MB (36.87%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+// Iterate over the array 3 + (4* 10) to get the full number and divide by ten adding the numbers to the array
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+
+    }
+}
