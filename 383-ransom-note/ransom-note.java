4ddd3595107dba736090c2efaff6716f36464437
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-10-20 12:17:07 -0700

    Time: 3 ms (77.38%) | Memory: 43.2 MB (97.64%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,25 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> seenNote = new HashMap<>();
-        Map<Character, Integer> seenMag = new HashMap<>();
-
-        for (char c : ransomNote.toCharArray()) {
-            seenNote.put(c, seenNote.getOrDefault(c, 0) + 1);
+        int a[] = new int[26];
+        
+        for(int i=0;i<magazine.length();i++){
+            a[magazine.charAt(i)-'a']++;
         }
-
-        for (char c : magazine.toCharArray()) {
-            seenMag.put(c, seenMag.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : ransomNote.toCharArray()) {
-            if (seenMag.get(c) == null || seenMag.get(c) < seenNote.get(c)) {
+        for(int i=0;i<ransomNote.length();i++){
+            if(--a[ransomNote.charAt(i)-'a']<0)
                 return false;
-            }
         }
-
         return true;
-
     }
 }
 
-// Time and space are both O(ransomNote.length() + magazine.length()) -> O(m + n)
+// Time O(ransomNote.length() + magazine.length()) -> O(m + n)
+// Space O(1) as there are only 26 letters in the alphabet and we know that all letters are lowercase
