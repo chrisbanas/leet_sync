22987cdb398a62d6050bc1a289b152dab3380232
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-10-24 14:35:50 -0700

    Time: 2 ms (95.34%) | Memory: 43.4 MB (44.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,27 +1,30 @@
+// All this problem cares about is returning the unique numbers that show up in each array.
+
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> uniqueNums1 = new HashSet<>();
+  public int[] intersection(int[] nums1, int[] nums2) {
+    Set<Integer> seen1 = new HashSet<>();
+    Set<Integer> seen2 = new HashSet<>();
 
-        for (int num : nums1) {
-            uniqueNums1.add(num);
-        }
-
-        List<Integer> pairs = new ArrayList<>();
-
-        for (int i = 0; i < nums2.length; i++) {
-            if (uniqueNums1.contains(nums2[i])) {
-                pairs.add(nums2[i]);
-                uniqueNums1.remove(nums2[i]); // You have to remove it from the set otherwise you end up with duplicates in the result.
-            }
-        }
-
-        int[] output = new int[pairs.size()];
-
-        for (int i = 0; i < output.length; i++) {
-            output[i] = pairs.get(i);
-        }
-
-         return output;
-
+    for (int num : nums1) {
+      seen1.add(num);
     }
+
+    for (int num : nums2) {
+      if (seen1.contains(num)) {
+        seen2.add(num);
+      }
+    }
+
+    int[] output = new int[seen2.size()];
+    int i = 0;
+
+    for (int num : seen2) {
+      output[i++] = num;
+    }
+
+    return output;
+
+  }
 }
+
+// Space & Time are O(n + m)
