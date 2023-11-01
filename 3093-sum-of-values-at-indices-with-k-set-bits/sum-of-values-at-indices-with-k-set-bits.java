decca7e15605d23c50a90ef18242c8a02f477f9a
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-01 13:41:03 -0700

    Time: 1 ms (100.00%) | Memory: 43.2 MB (71.51%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.java
@@ -1,25 +1,15 @@
 class Solution {
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
-        int output = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            String binaryString = Integer.toBinaryString(i);
-            int onesCount = 0;
-
-            for (int j = 0; j < binaryString.length(); j++) {
-                if (binaryString.charAt(j) == '1') {
-                    onesCount++;
-                }
-            }
-
-            if (onesCount == k) {
-                output += nums.get(i);
-            }
+        int temp=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(Integer.bitCount(i)==k)
+                temp+=nums.get(i);
+            
         }
-
-        return output;
+        return temp;
     }
 }
 
-// Time O(n log(n)) -> as the second loop is log2(n) or log(n)
+// Time O(n)
 // Space O(1)
