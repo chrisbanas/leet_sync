11050a45bd80fc449798e6a4c94738b9c71cfc28
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-02 16:09:52 -0700

    Time: 1 ms (85.12%) | Memory: 39.6 MB (46.68%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        // Base Case
-        if (n == 1) {
-            return true;
-        }
-
-        // Second Base Case
-        if (n == 0 || n % 2 != 0) {
+        if (n == 0) {
             return false;
         }
 
-        // Recursive Step
-        return isPowerOfTwo(n / 2);
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        return n % 2 == 0 || n == 1;
     }
 }
 
 // Time O(log(n))
-// Space O(n)
+// Space O(1)
