83b69986ef1a54e9dc2dadbb36dd61162ff40a75
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-02 14:54:16 -0700

    Time: 0 ms (100.00%) | Memory: 41 MB (87.18%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,40 +10,23 @@
  */
 class Solution {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-    if (list1 == null && list2 == null) {
-      return null;
-    } else if (list1 == null && list2 != null) {
+    // Base Case
+    if (list1 == null) {
       return list2;
-    } else if (list2 == null && list1 != null) {
+    }
+
+    if (list2 == null) {
       return list1;
     }
 
-    ListNode current1 = list1;
-    ListNode current2 = list2;
-
-    ListNode output = new ListNode();
-    ListNode dummy = output;
-
-    while (current1 != null && current2 != null) {
-      if (current1.val <= current2.val) {
-        dummy.next = current1;
-        current1 = current1.next;
-      } else {
-        dummy.next = current2;
-        current2 = current2.next;
-      }
-      dummy = dummy.next;
-    }
-
-    if (current1 == null) {
-      dummy.next = current2;
+    // Recursive Case
+    if (list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
     } else {
-      dummy.next = current1;
+      list2.next = mergeTwoLists(list1, list2.next);
+      return list2;
     }
-
-    return output.next;
 
   }
-
 }
