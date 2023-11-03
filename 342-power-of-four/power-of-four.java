8cbcbb18dd2dcb0e31555e1942e561373c93bb05
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-02 17:07:15 -0700

    Time: 1 ms (76.59%) | Memory: 40.1 MB (8.58%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n == 1) {
+            return true;
+        }
+
+        if (n <= 0 || n % 4 != 0) {
+            return false;
+        }
+
+        return isPowerOfFour(n / 4);
+    }
+}
+
+// Time O(log(n))
+// Space O(n)
