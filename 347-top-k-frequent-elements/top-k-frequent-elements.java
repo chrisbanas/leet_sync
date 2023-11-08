0c4467603464bf16d0c392db7692c34dc98f1665
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-08 11:26:19 -0800

    Time: 13 ms (72.27%) | Memory: 47.3 MB (76.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+
         Map<Integer, Integer> seen = new HashMap<>();
 
         for (int num : nums) {
             seen.put(num, seen.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        List<Integer>[] bucket = new List[nums.length + 1];
 
-        for (Map.Entry<Integer, Integer> entry : seen.entrySet()) {
-            maxHeap.add(entry);
+        for (int key : seen.keySet()) {
+            int frequency = seen.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
         }
 
         int[] output = new int[k];
+        int count = 0;
 
-        for (int i = 0; i < output.length; i++) {
-            output[i] = maxHeap.poll().getKey();
+        for (int i = bucket.length - 1; i > 0 && count < k; i--) {
+            if (bucket[i] != null) {
+                for(Integer j : bucket[i]) {
+                    output[count] = j;
+                    count++;
+                }
+            }
         }
 
         return output;
     }
 }
 
-// Time O(n log(n))
+// Time O(n)
 // Space O(n)
