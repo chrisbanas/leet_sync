35228e835c6a4a22f1b6cdf56b961e2e388ba794
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-12 11:41:04 -0800

    Time: 1 ms (79.95%) | Memory: 41.3 MB (24.84%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,35 +1,42 @@
 class Solution {
-     public List<String> letterCombinations(String digits) {
-        LinkedList<String> output = new LinkedList<>();
+    private List<String> combinations = new ArrayList<>();
+    private Map<Character, String> letters = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl",
+            '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+    private String phoneDigits;
 
+    public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
-            return output;
+            return combinations;
         }
 
-        output.add("");
+        phoneDigits = digits;
+        dfsBackTrack(0, new StringBuilder());
+        return combinations;
 
-        String[] charMap = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    }
 
-        for (int i = 0; i < digits.length(); i++) {
-            int j = Character.getNumericValue(digits.charAt(i));
-
-            while (output.peek().length() == i) {
-                StringBuilder permutation = new StringBuilder(output.remove());
-
-                for (char c : charMap[j].toCharArray()) {
-                    output.add(String.valueOf(permutation) + c);
-                }
-            }
+    private void dfsBackTrack(int i, StringBuilder path) {
+        if (path.length() == phoneDigits.length()) {
+            combinations.add(path.toString());
+            return;
         }
 
-        return output;
+        String possibleLetters = letters.get(phoneDigits.charAt(i));
+
+        for (char letter : possibleLetters.toCharArray()) {
+            path.append(letter);
+            dfsBackTrack(i + 1, path);
+            path.deleteCharAt(path.length() - 1);
+        }
+
     }
 }
 
 /* 
 
-BFS
+DFS
 
-Both the time and space complexity are O(k^n), where k is the average number of characters a digit can represent, and n is the length of the input string. 
+The time complexity is how many combinations we can have. So test one is 9 as 2 & 3 have 3 letters each, so 3 * 3
+The length of the output stings will be the same as the input strings.
 
 */
