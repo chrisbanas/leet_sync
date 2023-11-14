a6de123bcabeab2482556e5e757be134abbe8f4b
Christopher Banas <123133713+chrisbanas@users.noreply.github.com>
2023-11-13 17:37:20 -0800

    Time: 0 ms (100.00%) | Memory: 39.7 MB (58.91%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,7 +15,6 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
         if (p == null && q == null) {
             return true;
         }
@@ -24,13 +23,35 @@ class Solution {
             return false;
         }
 
-        if (p.val != q.val) {
-            return false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+
+            if (node1 == null && node2 == null) {
+                continue;
+            }
+
+            if (node1 == null || node2 == null) {
+                return false;
+            }
+
+            if (node1.val != node2.val) {
+                return false;
+            }
+
+            queue.add(node1.left);
+            queue.add(node2.left);
+            queue.add(node1.right);
+            queue.add(node2.right);
         }
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
     }
 }
 
 // Time O(n)
-// Space O(n)
+// Space O(m + n)
